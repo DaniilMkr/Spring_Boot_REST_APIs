e84ec4602176ca2935ae4d7d59c3f0c5fba88958
Daniil Makarov <daniilmakarovmk@gmail.com>
2023-04-30 14:59:41 +0200

    create REST API for List of Students

--- springboot-rest-api/src/main/java/com/javaguides/springboot/controller/StudentController.java
+++ springboot-rest-api/src/main/java/com/javaguides/springboot/controller/StudentController.java
@@ -4,6 +4,9 @@ import com.javaguides.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
@@ -14,5 +17,15 @@ public class StudentController {
     public Student getStudent() {
         Student student = new Student(1, "Daniil", "Makarov");
         return student;
+    }
+
+    // http://localhost:8090/students
+    @GetMapping("students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Andrey", "Makarov"));
+        students.add(new Student(2, "Daniil", "Makarov"));
+        students.add(new Student(3, "Anna", "Makarova"));
+        return students;
     }
 }
